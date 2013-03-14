e9f56225194c0157d9f3ed81a9faef8793a31b5e
Keshav Kini <keshav.kini@gmail.com>
2013-03-14 16:21:35 -0700

    boop

--- Puzzle.hs
+++ Puzzle.hs
@@ -4,10 +4,9 @@ import Data.Maybe
 import Data.List
 import Data.Vector ((//), (!), Vector)
 import qualified Data.Vector as V
-import Data.Graph.AStar
-import qualified Data.Set  as S
 
--- Backwards recursive Steinhaus-Johnson-Trotter algorithm
+-- Backwards recursive Steinhaus-Johnson-Trotter algorithm (see
+-- writeup.rst for more info)
 nthPermutation :: Int -> [a] -> [a]
 nthPermutation 0 [] = []
 nthPermutation n l@(x:xs) = insertZigZag n x $
@@ -26,15 +25,16 @@ nthPermutation n l@(x:xs) = insertZigZag n x $
           | otherwise
             = zigLength - (offset' - zigLength) - 1
 
+-- The main board data type. It's stored as a one dimensional vector
+-- of Maybe Int values.
 data Board = Board Int (Vector (Maybe Int)) deriving (Eq)
 
-instance Ord Board where
-  compare (Board _ v1) (Board _ v2) = compare v1 v2
-
+-- This allows us to print boards easily. The code looks ugly but the
+-- display comes out looking nice :)
 instance Show Board where
   show (Board i v)
     | sort (map isNothing l) /= take 15 (repeat False) ++ [True]
-      || isJust (l !! i)
+      || isJust (l !! i)    -- sanity check
       = error "Broken Board object!"
     | otherwise
       = unlines [ ""
@@ -56,6 +56,9 @@ instance Show Board where
       pad p@[c, c'] = p
       set xs = "| " ++ intercalate " | " (map (pad . show') xs) ++ " |"
 
+-- This function takes a list of the numbers from 1 through 15 in some
+-- order, and a position at which to insert the empty space, and creates
+-- a board from that information.
 makeBoardFromList :: [Int] -> Int -> Board
 makeBoardFromList nums emptyPos =
   Board emptyPos $ V.fromList
@@ -64,7 +67,8 @@ makeBoardFromList nums emptyPos =
     nums' = map Just nums
 
 -- There are 10461394944000 possible boards, which we can retrieve
--- quickly by their ID
+-- quickly by their ID. See writeup.rst for more information about how
+-- this works.
 makeBoardFromID :: Int -> Board
 makeBoardFromID n = makeBoardFromList (nthPermutation i [1..15]) j'
   where
@@ -75,61 +79,89 @@ makeBoardFromID n = makeBoardFromList (nthPermutation i [1..15]) j'
     j' | even i = evens !! j
        | odd  i = odds  !! j
 
+-- This, board #0, is the "perfect" board, i.e. the one which the player
+-- tries to reach.
 perfectBoard :: Board
 perfectBoard = makeBoardFromID 0
 
+-- Find the Manhattan distance between two points in the puzzle, as
+-- indexed linearly according to our data structure
 manhattan :: Int -> Int -> Int
 manhattan a b = abs (mod a 4 - mod b 4) +
                 abs (div a 4 - div b 4)
 
 
+-- This is an enumerated data type representing possible moves you can
+-- make. The direction U/D/L/R refers to which direction the space, not
+-- the piece, moves.
 data Move = MvU | MvD | MvL | MvR deriving (Enum, Eq, Ord)
 
+-- This is easier to read than the constructor names
 instance Show Move where
   show MvU = "↑"
   show MvD = "↓"
   show MvL = "←"
   show MvR = "→"
 
+-- How to find the piece to move into the space, given a direction for
+-- the space to move
 moveOffset :: Move -> Int
 moveOffset MvU = -4
 moveOffset MvD = 4
 moveOffset MvL = -1
 moveOffset MvR = 1
 
+-- Opposite directions mapping
 moveOpposite :: Move -> Move
 moveOpposite MvU = MvD
 moveOpposite MvD = MvU
 moveOpposite MvL = MvR
 moveOpposite MvR = MvL
 
+-- Check whether a move would go out of bounds from a given position
 canMoveFrom :: Int -> Move -> Bool
 canMoveFrom i MvU = i `div` 4 /= 0
 canMoveFrom i MvD = i `div` 4 /= 3
 canMoveFrom i MvL = i `mod` 4 /= 0
 canMoveFrom i MvR = i `mod` 4 /= 3
 
+-- Find what move would go from the first board to the second, if any.
+-- Not a super necessary function, but was useful for some debugging
+-- code...
 diffBoards :: Board -> Board -> Maybe Move
 diffBoards b1 b2 = find ((== b2) . flip makeMove b1) [MvU, MvD, MvL, MvR]
 
+-- Check what moves are possible from the current state.
 possibleMoves :: Board -> [Move]
 possibleMoves (Board i _) = filter (canMoveFrom i) [MvU, MvD, MvL, MvR]
 
+-- Find out how a board would look after making a given move.
 makeMove :: Move -> Board -> Board
 makeMove m (Board i v)
   = Board (i+d) $ v // [(i+d, Nothing), (i, v ! (i+d))]
   where d = moveOffset m
 
+
+-- A type for restrictions on the game state which we might want to
+-- abide by during AI search.
 type Restriction = (Board -> Bool)
 
+-- The most common kind of restriction - the fixing of a certain piece
+-- in its current position.
 fixPos :: Board -> Int -> Restriction
 fixPos (Board _ v) i (Board _ v') = v' ! i == v ! i
 
 
 -- AI stuff
+
+-- Check what moves are possible from the current state, while also
+-- obeying certain restrictions.
 possibleMoves' :: Board -> [Restriction] -> [Move]
 possibleMoves' b@(Board i v) rs
   = filter (\move -> and (map ($ makeMove move b) rs)) $ possibleMoves b
+
+-- See writeup.rst for more info about the following stuff, which
+-- doesn't work yet.
 
 -- findShiftSequence :: (Int, Move) -> Board -> [Restriction] ->
 --                      Maybe (Board, [Move])
@@ -212,6 +244,7 @@ possibleMoves' b@(Board i v) rs
 --   where
 --     getAdj b = S.fromList $ map (flip makeMove b) $ possibleMoves b []
 
+---- Test
 --main = do
 --  x <- readLn
 --  print $ solve $ makeBoardFromID x
